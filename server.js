26696d18c5f224f705fb3d9531725370bab54622
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2017-10-17 18:04:08 +0200

    /speakers endpoint

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const logger = require("koa-logger");
 const serve = require("koa-static");
 const path = require("path");
 
+const data = require("./data/data");
+
 const app = new Koa();
 const router = new Router();
 
@@ -14,16 +16,19 @@ app.use(logger());
 app.use(router.routes()).use(router.allowedMethods());
 
 // Route /root
-router.get("/", async function (ctx) {
-  ctx.body = {serverTime: new Date()}
+router.get("/", async function(ctx) {
+  ctx.body = { serverTime: new Date() };
 });
 
 // Route /ping
-router.get("/ping", async function (ctx) {
-ctx.body = "pong"
+router.get("/ping", async function(ctx) {
+  ctx.body = "pong";
+});
+
+router.get("/speakers", async function(ctx){
+  ctx.body = data.speakers;
 });
 
 app.use(compress());
 app.listen(9000);
 console.log("listening on port 9000");
-
